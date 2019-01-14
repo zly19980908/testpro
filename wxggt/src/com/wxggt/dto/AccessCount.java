9a1e391666c5b8cbe2502e0751d2833efbdbf5ae
wx <1935428967@qq.com>
2019-01-14 15:48:59 +0800

    这是我的第一次提交

---
+++ wxggt/src/com/wxggt/dto/AccessCount.java
@@ -0,0 +1,34 @@
+package com.wxggt.dto;
+
+import java.util.Date;
+
+public class AccessCount {
+	private int aid;
+	private Date aDate;
+	private int accessCount;
+
+	public int getAid() {
+		return aid;
+	}
+
+	public void setAid(int aid) {
+		this.aid = aid;
+	}
+
+	public Date getaDate() {
+		return aDate;
+	}
+
+	public void setaDate(Date aDate) {
+		this.aDate = aDate;
+	}
+
+	public int getAccessCount() {
+		return accessCount;
+	}
+
+	public void setAccessCount(int accessCount) {
+		this.accessCount = accessCount;
+	}
+
+}
