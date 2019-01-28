0d5ad2a855b5638bf4d447aaef7dc55c44b4222e
zhuchangshaonv <alwayChina@gmail.com>
2019-01-28 23:21:46 +0800

    action和servlet共存测试

---
+++ wxggt/src/com/wxggt/action/TestAction.java
@@ -0,0 +1,11 @@
+package com.wxggt.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class TestAction extends ActionSupport {
+	private static final long serialVersionUID = 1L;
+
+	public String execute() throws Exception {
+		return SUCCESS;
+	}
+}
