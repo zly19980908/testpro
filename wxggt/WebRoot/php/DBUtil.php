8798ee463282d7bdda58601c32dd1cbb01d2af22
zhuchangshaonv <alway@alway-PC.lan>
2019-02-16 11:45:09 +0800

    乱码问题

--- wxggt/WebRoot/php/DBUtil.php
+++ wxggt/WebRoot/php/DBUtil.php
@@ -1,9 +1,9 @@
 <?php
-/* Á¬½ÓÊý¾Ý¿â */
+/* è¿žæŽ¥æ•°æ®åº“ */
 $link = mysqli_connect ( '120.79.92.84', 'root', 'root', 'xhxt?useSSL=false' );
 if (! $link) {
 	printf ( "Can't connect to MySQL Server. Errorcode: %s ", mysqli_connect_error () );
 	exit ();
 }
-mysqli_query ( $link, 'set names gb2312' ); /* ½â¾öÖÐÎÄÂÒÂëµÄÎÊÌâ */
+mysqli_query ( $link, 'set names gb2312' ); /* è§£å†³ä¸­æ–‡ä¹±ç çš„é—®é¢˜ */
 ?>
--- wxggt/WebRoot/php/registered.php
+++ wxggt/WebRoot/php/registered.php
@@ -3,19 +3,19 @@ include('DBUtil.php');
 $q = $_GET ['tno'];
 $sql="SELECT COUNT(*) FROM teacherinfo WHERE tno='$q'";
 	if ($result = mysqli_query ( $link, $sql )) {
-		/* ½«½á¹û´æÔÚMy_arrayÖÐ */
+		/* å°†ç»“æžœå­˜åœ¨My_arrayä¸­ */
 		$My_array = array ();
-		/* ·µ»Ø²éÑ¯µÄ½á¹û */
+		/* è¿”å›žæŸ¥è¯¢çš„ç»“æžœ */
 		while ( $row = mysqli_fetch_assoc ( $result ) ) {
-			/* ·ÀÖ¹ÖÐÎÄÂÒÂë */
+			/* é˜²æ­¢ä¸­æ–‡ä¹±ç  */
 			$data = $row ['COUNT(*)'];
 		}
-		/* ½áÊø²éÑ¯ÊÍ·ÅÄÚ´æ */
+		/* ç»“æŸæŸ¥è¯¢é‡Šæ”¾å†…å­˜ */
 		mysqli_free_result ( $result );
 	}
 
-/*´«Öµ*/
+/*ä¼ å€¼*/
    echo json_encode ( $data );
-/* ¹Ø±ÕÁ¬½Ó */
+/* å…³é—­è¿žæŽ¥ */
 mysqli_close ( $link );
 ?>
